7e07fbf4f06fcd93ac2da9c78e4605cd74a42c83
Aditya <adityas.singh.tech@gmail.com>
2026-03-19 13:49:30 +0530

    remove demo file from repo

--- app/demo/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AnimatedBorderButton } from "@/registry/animated-border";
-
-export default function DemoPage() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-black">
-      <AnimatedBorderButton>Animated Border</AnimatedBorderButton>
-    </div>
-  );
-}
